df4b107e1a1d282328bc459128f4315063cca53c
lawrence stent <lawrencestent@gmail.com>
2015-03-09 11:25:03 +0200

    fourth update
    removed unnecessary file. moved js code in index.html to video.js.

--- video.js
+++ video.js
@@ -1,41 +1,142 @@
-
-
-
 jQuery(document).ready(function() {
-	$("#sb").click(function() {
-		$("p").hide();
+		$("#but").click(function() {
 
-		alert("first");
-		//var inval = getelementbyId("url");
-		//var url = $inval.text();
+			var url = $("#url").val();
+			//var str = texting.text();
 
-		//prependbody(url);
+			alert(url);
+			getJSONdata(url);
 
-		//$(".div").prepend().html("<iframe src=" + url +"><iframe>");
+			//prependbody(texting);
+
+					});
 	});
-});
 
-function clickme()
-{
+	//what needs to be done: for post -  accept user url input, send that input to function whereby $.getJSON responds with the api info. extract the video id, title, description, url... 
 
-	$(document).ready(function() {
-		$("#two").click(function(event) {
-			alert("surprise");
+	//send that information in json format to an external file. 
+
+	//on load of the page any videos in the json file are read on to the page and put into a list//table under the correct columns
+	//});
+
+	//method to load the json file data
+	function onload()
+	{
+		var xmlhttp = new XMLHttpRequest();
+		var urli = "data.txt";
+
+		xmlhttp.onreadystatechange = function()
+		{
+		    if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+		    {
+		    	//calls the readJson method to place data in a table
+		    	var jtext = $.parseJSON(xmlhttp.responseText);
+		        readJson(jtext);
+	    	}
+		}
+
+		xmlhttp.open("GET", urli, true);
+		xmlhttp.send(null);
+	}
+
+	//method for the output of json file data
+	function readJson(jsontext)
+	{
+		var jarray = $.parseJSON(jsontext);
+		var i = 0;
+
+		//write directly into a table on page load
+		var html = "<table class='table table-hover'><thead><tr><th>ID</th><th>TITLE</th><th>DESCRIPTION</th><th>Delete video</th></tr></thead>";
+
+		for (i; i < jarray.length; i++) 
+		{
+			html += "<tr><td>"+
+			jarray[i].id+"</td><td><a href='"+jarray[i].url+"'"+
+			jarray[i].title+"</a></td><td>"+
+			jarray[i].description+"</td>"+
+			//call function jquery delete with id...delete jarray[i]['id'];
+			//problem with below line of code - may not read the correct id as this is on page load to get id may 
+			"<td id='"+i+"' onclick='deletejson(this.id, "+jarray+");'>DELETE</td></tr>";
+		}
+
+		html +="</table>"
+		document.getelementbyId("jsondata").innerhtml = html;
+	}
+
+	//method to accept yt api information and convert to json
+	function getJSONdata(urlstring)
+	{
+		var id = "";
+		var title = "";
+		var description = "";
+
+		$.getJSON(urlstring, function(response) {
+				id = response.data.id;
+				title = response.data.title;
+				description = response.data.description;
+
+				alert(id);
 		});
-	});
-}
 
-function prependbody(url)
-{
-	var vid = $("<iframe />").attr({
-		src: url,
-		width:"560",
-		height:"349",
-		frameborder="0",
-		allowfullscreen
-	});
+		var inputData = {
+					"id" : id,
+					"title" : title,
+					"description" : description,
+					"url" : urlstring
+				}
 
-	alert(url);
+		//sends json to function
+		writeJson(inputData);
+	}
 
-	$(".videowrap").prepend(vid);
-}
+	//this function is to accept the Youtube response data and write it to our json file.
+	function writeJson(ytdata)
+	{
+		//here i find infile = /file.json in js. to write to file in json. possibly use writeJson();
+
+		//$.writeJson("")
+		var obj = JSON.stringify(ytdata);
+
+		/*$.post('data.json', obj, function(data, textStatus) 
+		{
+			alert(obj);
+		});*/
+		$.ajax({
+			url: 'data.json',
+			type: 'POST',
+			dataType: 'json',
+			data: obj,
+		})
+		.done(function() {
+			console.log("success");
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+		
+	}
+
+	//this is to accept the json id and delete it from file
+	function deletejson(id, jsonarray)
+	{
+		var deleting = document.getelementbyId(id);
+
+		$.ajax({
+			url: 'data.json',
+			type: 'DELETE',
+			dataType: 'json',
+			data: {jsonarray[id]}
+		})
+		.done(function() {
+			console.log("success");
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+	}
